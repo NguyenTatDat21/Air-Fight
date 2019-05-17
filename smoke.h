1aed2d42943bf7b5d578d7b4277fa3faca89bc4f
Dat Nguyen <tpc2102@gmail.com>
2019-05-17 12:00:26 +0700

    some rèactor

--- smoke.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "object.h"
-
-class smoke : public object {
-    public :
-        int time;
-        smoke(sf::Vector2f _pos, sf::Vector2f _facing);
-        void update();
-};
